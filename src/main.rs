e7d14512a3ecb2e87eeecec4ae76a59e6725fa44
AlvesKZ <igoralvex01@gmail.com>
2025-06-06 10:25:46 -0300

    Create GET controllers

--- src/main.rs
+++ src/main.rs
@@ -87,6 +87,43 @@ fn handle_post_request(request: &str) -> (String, String) {
     }
 }
 
+handle_get_request(request: &str) -> (String, String) {
+   match (get_id(request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+       (Ok(id), Ok(mut client)) => {
+           match client.query("SELECT * FROM users WHERE id = $1", &[&id]) {
+               Ok(row) => {
+                   let user: User = User {
+                       id: Some(row.get(0)),
+                       name: row.get(1),
+                       email: row.get(2),
+                   };
+                   (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap())
+               }
+               Err(_) => (NOT_FOUND.to_string(), "User not found".to_string()),
+           }
+       }
+       _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+   }
+}
+
+fn handle_get_all_request(request: &str) -> (String, String) {
+    match Client::connect(DB_URL, NoTls) {
+        Ok(mut client) => {
+            let mut users = Vec::new();
+            
+            for row in client.query("SELECT * FROM users", &[]).unwrap() {
+                users.push(User {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                });
+            }
+            (OK_RESPONSE.to_string(), serde_json::to_string(&users).unwrap())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    }
+}
+
 fn set_database() -> Result<(), PostgresError> {
     let mut client = Client::connect(DB_URL, NoTls)?;
     client.execute(
