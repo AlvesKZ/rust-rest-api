c3ad1e7e98e574914d0db02aecbe44c1307085bb
AlvesKZ <igoralvex01@gmail.com>
2025-06-05 22:37:02 -0300

    Get request user and id

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use postgres::{Client, NoTls};
 use postgres::Error as PostgresError;
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use postgres::{Client, NoTls};
 use std::env;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 #[macro_use]
-extern crate serde_derive:
+extern crate serde_derive;
 
 #[derive(Serialize, Deserialize)]
 struct User {
     id: Option<i32>,
     name: String,
-    email: String
+    email: String,
 }
 
-const DB_URL: &str = !env("DATABASE_URL");
+const DB_URL: &str = env!("DATABASE_URL");
 
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-const INTERNAL_SERVER_ERROR: "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
 
 fn main() {
     if let Err(e) = set_database() {
@@ -28,17 +28,43 @@ fn main() {
 
     let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
     println!("Server running on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
 
 fn set_database() -> Result<(), PostgresError> {
     let mut client = Client::connect(DB_URL, NoTls)?;
-    
-    client.execute("
+    client.execute(
+        "
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             name VARCHAR NOT NULL,
             email VARCHAR NOT NULL UNIQUE
-        )", 
-        &[]
+        )",
+        &[],
     )?;
+    Ok(())
+}
+
+fn get_id(request: &str) -> &str {
+    request
+        .split('/')
+        .nth(2)
+        .unwrap_or_default()
+        .split_whitespace()
+        .next()
+        .unwrap_or_default()
+}
+
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
 }
